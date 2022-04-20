7e304a37c52d3508ce1f54d7ea3f52b20a56265a
Your Name <you@example.com>
2022-04-20 20:15:13 +0800

    optimizing page

--- pages/_app.js
+++ pages/_app.js
@@ -5,15 +5,25 @@ import { ApolloProvider } from '@apollo/client';
 import client from '../src/apollo/client';
 import { ChakraProvider } from '@chakra-ui/react';
 import theme from '../theme';
+// import dynamic from 'next/dynamic';
+
+// import {
+//   ThemeProvider,
+//   CSSReset,
+//   ColorModeProvider
+// } from '@chakra-ui/core'
 
 function MyApp({ Component, pageProps }) {
-    return (
-        <ChakraProvider theme={theme}>
-            <ApolloProvider client={client}>
-                <Component {...pageProps} />
-            </ApolloProvider>
-        </ChakraProvider>
-    );
+
+  // const ChakraProvider = dynamic(() => import('@chakra-ui/react').then(module => module.ChakraProvider))
+
+  return (
+    <ChakraProvider theme={theme}>
+      <ApolloProvider client={client}>
+        <Component {...pageProps} />
+      </ApolloProvider>
+    </ChakraProvider>
+  );
 }
 
 export default MyApp;
--- pages/_document.js
+++ pages/_document.js
@@ -1,17 +1,28 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+// import { renderStatic } from '../src/shared/renderer';
+import { extractCritical } from "@emotion/server";
 
 export default class MyDocument extends Document {
-    render() {
-        return (
-            <Html lang="en">
-                <Head></Head>
-                <body>
-                    <Main />
-                    <NextScript />
-                </body>
-            </Html>
-        );
-    }
+  static async getServerSideProps(ctx) {
+    const page = await ctx.renderPage()
+    const initialProps = await Document.getInitialProps(ctx);
+    const styles = extractCritical(page.html);
+
+    console.log(styles)
+    return { ...initialProps, ...page, ...styles };
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head></Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
